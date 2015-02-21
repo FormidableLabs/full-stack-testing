e15b4f8358fd726861ecd98f2758a700baa48efd
Ryan Roemer <ryan@loose-bits.com>
2015-02-21 06:45:14 -0800

    Temp work.

--- client/app.js
+++ client/app.js
@@ -5,7 +5,14 @@ var $ = require("jquery");
 require("bootstrap"); // Empty require to enable bootstrap JS
 
 $(function () {
-  $("<code style='font-size: 2em' />")
-    .text("TODO: Write an App!")
-    .appendTo($("<div style='margin-top: 20px' />").appendTo($(".container")));
+  var $output = $("<code style='font-size: 2em' />")
+  $output.text("TODO: Write an App!")
+  $output.appendTo($("<div style='margin-top: 20px' />")
+    .appendTo($(".container")));
+
+  $(".js-convert").on("click", function (ev) {
+    ev.preventDefault();
+    var convertType = $(ev.currentTarget).data("convert");
+    console.log("TODO HERE CONVERT TYPE", convertType);
+  });
 });
