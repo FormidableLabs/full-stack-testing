bd2cc33c43d7e1ffc85a2da5ff17104506a6b132
Ryan Roemer <ryan@loose-bits.com>
2015-02-23 16:36:27 -0800

    Decompose frontend more.

--- client/app.js
+++ client/app.js
@@ -5,51 +5,22 @@ var $ = require("jquery");
 require("bootstrap"); // Empty require to enable bootstrap JS
 
 var Form = require("./form");
+var Converter = require("./converter");
 
 $(function () {
-  // Stash selectors.
-  var $input = $(".js-input");
-  var $output = $(".js-output");
-
   // Bind form and initialize UI state.
   var form = new Form({
-    $input: $input,
+    $input: $(".js-input"),
     $convertLabel: $(".js-convert-label"),
     $convertTypes: $(".js-convert")
   });
 
-  // Perform the actual conversion request and UI update.
-  var _convertText = function () {
-    var input = $input.val();
-
-    // TODO[RYAN]: Switch to real UI.
-    // TODO[RYAN]: Decompose better?
-    var template = function (title, content) {
-      return $(
-        "<div class='panel panel-default'>" +
-        "  <div class='panel-heading'>" +
-        "    <h3 class='panel-title'>" + title + "</h3>" +
-        "  </div>" +
-        "  <div class='panel-body'>" + content + "</div>" +
-        "</div>"
-      );
-    };
-
-    $output.empty();
-
-    // Iterate all conversion types, make AJAX request and update UI.
-    $.each(form.convertTypes, function (i, type) {
-      $.get("/api/" + type, { from: input }, function (data) {
-        $output.append(template(type, data.to));
-      });
-    });
-  };
-
-  // Listen and submit action.
-  $(".js-action").click(_convertText);
-  $(".js-input").keydown(function (ev) {
-    if (ev.which === 13 /* Enter key */) {
-      _convertText();
-    }
+  // Bind converter and initialize.
+  /*eslint-disable no-new*/
+  new Converter({
+    form: form,
+    $output: $(".js-output"),
+    $action: $(".js-action")
   });
+  /*eslint-enable no-new*/
 });
--- client/converter.js
+++ client/converter.js
@@ -1,3 +1,57 @@
 /**
  * UI: Converter
  */
+var $ = require("jquery");
+
+/**
+ * Converter wrapper.
+ *
+ * @param {Object} opts                 options object
+ * @param {Object} opts.form            Form UI object wrapper
+ * @param {Object} opts.$output         output DOM element
+ * @param {Object} opts.$action         action selector
+ */
+var Converter = module.exports = function (opts) {
+  var self = this;
+
+  // Set options.
+  this.form = opts.form;
+  this.$output = opts.$output;
+  this.$action = opts.$action;
+
+  // Listen and submit action.
+  this.$action.click(function () { self.convert(); });
+  this.form.$input.keydown(function (ev) {
+    if (ev.which === 13 /* Enter key */) {
+      self.convert();
+    }
+  });
+};
+
+// Create HTML with title and content.
+Converter.prototype.template = function (title, content) {
+  return $(
+    "<div class='panel panel-default'>" +
+    "  <div class='panel-heading'>" +
+    "    <h3 class='panel-title'>" + title + "</h3>" +
+    "  </div>" +
+    "  <div class='panel-body'>" + content + "</div>" +
+    "</div>"
+  );
+};
+
+// Retrieve data and update UI.
+Converter.prototype.convert = function () {
+  var self = this;
+  var input = this.form.$input.val();
+
+  // Clear out existing data.
+  this.$output.empty();
+
+  // Iterate all conversion types, make AJAX request and update UI.
+  $.each(this.form.convertTypes, function (i, type) {
+    $.get("/api/" + type, { from: input }, function (data) {
+      self.$output.append(self.template(type, data.to));
+    });
+  });
+};
