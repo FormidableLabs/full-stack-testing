290c146a183488ee7d491c366402b5eb1b81bf5f
Ryan Roemer <ryan@loose-bits.com>
2015-02-23 16:45:02 -0800

    Remove jquery wait

--- client/app.js
+++ client/app.js
@@ -7,20 +7,19 @@ require("bootstrap"); // Empty require to enable bootstrap JS
 var Form = require("./form");
 var Converter = require("./converter");
 
-$(function () {
-  // Bind form and initialize UI state.
-  var form = new Form({
-    $input: $(".js-input"),
-    $convertLabel: $(".js-convert-label"),
-    $convertTypes: $(".js-convert")
-  });
-
-  // Bind converter and initialize.
-  /*eslint-disable no-new*/
-  new Converter({
-    form: form,
-    $output: $(".js-output"),
-    $action: $(".js-action")
-  });
-  /*eslint-enable no-new*/
+// Our JS bundle loads after DOM, so don't need jQuery ready wait...
+// Bind form and initialize UI state.
+var form = new Form({
+  $input: $(".js-input"),
+  $convertLabel: $(".js-convert-label"),
+  $convertTypes: $(".js-convert")
 });
+
+// Bind converter and initialize.
+/*eslint-disable no-new*/
+new Converter({
+  form: form,
+  $output: $(".js-output"),
+  $action: $(".js-action")
+});
+/*eslint-enable no-new*/
