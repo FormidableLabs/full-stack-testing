c6cc7acbb18bd4e5cc2e860936123162158600ec
Ryan Roemer <ryan@loose-bits.com>
2015-02-24 22:39:36 -0800

    Refine camel, implement snake

--- server/converter.js
+++ server/converter.js
@@ -1,10 +1,18 @@
 /**
  * Convert strings!
+ *
+ * Common behavior:
+ *
+ * - Strip leading / trailing spaces.
+ * - Internal spaces are treated as a delimeter.
+ * - Collapse multiple occurences of delimeter.
  */
 
 /**
  * Camel case a string.
  *
+ *     myString -> myString
+ *     mySTring -> mySTring
  *     my_string -> myString
  *     my-string -> myString
  *
@@ -12,22 +20,32 @@
  * @returns {String}      camel-cased string
  */
 function camel(val) {
-  return (val || "").replace(/[-_](.)/g, function (m, first) {
-    return first.toUpperCase();
-  });
+  return (val || "")
+    .replace(/^\s+|\s+$/g, "")
+    .replace(/[-_ ]+(.)/g, function (m, first) {
+      return first.toUpperCase();
+    });
 }
 
 /**
  * Snake case a string.
  *
  *     myString  -> my_string
+ *     mySTring  -> my_string
+ *     my_string -> my_string
  *     my-string -> my_string
  *
  * @param   {String} val  string to convert
  * @returns {String}      snake-cased string
  */
 function snake(val) {
-  return "TODO: snake " + val;
+  return (val || "")
+    .replace(/^\s+|\s+$/g, "")
+    .replace(/([a-z])([A-Z])/, function (m, first, second) {
+      return first + "-" + second;
+    })
+    .split(/[-_ ]+/).join("-")
+    .toLowerCase();
 }
 
 /**
@@ -35,6 +53,7 @@ function snake(val) {
  *
  *     myString  -> my-string
  *     my_string -> my-string
+ *     my-string -> my-string
  *
  * @param   {String} val  string to convert
  * @returns {String}      dasherized string
