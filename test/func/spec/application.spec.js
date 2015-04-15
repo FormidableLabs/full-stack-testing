57d8e1718372a72f4b674db7bc0d36fd75e0bda8
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-04-14 17:27:19 -0700

    Start server and concat coverage results.

--- test/func/spec/application.spec.js
+++ test/func/spec/application.spec.js
@@ -1,4 +1,21 @@
+var app = require("../../../server");
+var PORT = process.env.FUNC_PORT || 3003;
+var server;
+
 describe("func/application", function () {
+
+  // Start up (and later stop) a single instance of the server so that we can
+  // interact with the web application via our tests.
+  // *Note*: Often you will need to reset data / server state to a known
+  // starting point in `beforeEach`/`afterEach` methods.
+  before(function (done) {
+    server = app.listen(PORT, done);
+  });
+
+  after(function (done) {
+    if (!server) { done(); }
+    server.close(done);
+  });
 
   describe("all the things", function () {
     // TODO[RYAN]
