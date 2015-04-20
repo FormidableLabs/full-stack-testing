734444d140d1a7a69d8326b3aaec822b2595881d
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-04-20 12:53:27 -0700

    Add setup to playgrounds.

--- test/func/spec/playground.spec.js
+++ test/func/spec/playground.spec.js
@@ -1,7 +1,41 @@
 /**
  * Playground: Functional Tests.
  */
+var app = require("../../../server");
+var PORT = process.env.FUNC_PORT || 3003;
+// var HOST = process.env.TEST_HOST || "http://127.0.0.1:" + PORT;
+
+var rowdy = require("rowdy");
+var adapter = rowdy.adapters.mocha;
+
 describe("YOUR_FUNCTIONAL_TEST_SUITE", function () {
+  var client;
+  var server;
+
+  // Mocha
+  this.timeout(10000);
+
+  // Selenium (WD.js/Rowdy) initialization
+  adapter.before();
+  adapter.beforeEach();
+  adapter.afterEach();
+  adapter.after();
+
+  before(function (done) {
+    client = adapter.client;
+    client
+      .setImplicitWaitTimeout(200)
+      .nodeify(done);
+  });
+
+  // Dev. Server
+  before(function (done) {
+    server = app.listen(PORT, done);
+  });
+  after(function (done) {
+    if (!server) { return done(); }
+    server.close(done);
+  });
 
   it("YOUR_FUNCTIONAL_TEST", function () {
     // YOUR_CODE
--- test/server/rest/playground.spec.js
+++ test/server/rest/playground.spec.js
@@ -1,7 +1,19 @@
 /**
  * Playground: Server REST Tests.
  */
+var app = require("../../../server");
+var PORT = process.env.TEST_PORT || 3002;
+
 describe("YOUR_SERVER_REST_TEST_SUITE", function () {
+  var server;
+
+  before(function (done) {
+    server = app.listen(PORT, done);
+  });
+  after(function (done) {
+    if (!server) { return done(); }
+    server.close(done);
+  });
 
   it("YOUR_SERVER_REST_TEST", function () {
     // YOUR_CODE
