15f1c30d656f9f3d9c33d4d8ef4dce6014b97b2d
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-04-20 22:50:29 -0700

    Update rowdy and remove manual configuration.

--- test/func/setup.js
+++ test/func/setup.js
@@ -6,12 +6,5 @@ var chai = require("chai");
 // Add test lib globals.
 global.expect = chai.expect;
 
-// Configure Rowdy.
-var rowdy = require("rowdy");
-// Recommendation is to actually customize your configuration by copying and
-// editing the example Rowdy config to `test/func/config.js`.
-var config = require("rowdy/examples/mocha/config");
-rowdy(config);
-
 // Set test environment
 process.env.NODE_ENV = "func";
